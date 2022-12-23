02dee24228b7de66d716cdb332970c5267c8b608
Omendiff <79208208+Omendiff@users.noreply.github.com>
2022-12-24 05:55:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cvizqxos8p8v.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lowchoonkeat-employee"
+customregion = "us-east-1"
