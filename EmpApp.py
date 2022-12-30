a0cafc790b2fbf971825ebe17ce4b075fc54a184
hongy <datedac@gmail.com>
2022-12-31 02:34:25 +0800

    ds

--- EmpApp.py
+++ EmpApp.py
@@ -32,6 +32,10 @@ def about():
 def getemp():
     return render_template('GetEmp.html')
 
+@app.route("/upemp/", methods=['GET', 'POST'])
+def upemp():
+    return render_template('UpEmp.html')
+
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
     emp_id = request.form['emp_id']
@@ -98,7 +102,6 @@ def getEmp():
          cursor.execute(select_stmt, { 'emp_id': int(emp_id) })
          for result in cursor:
             print(result)
-        
 
      except Exception as e:
         return str(e)
@@ -106,13 +109,79 @@ def getEmp():
      finally:
         cursor.close()
 
-     return render_template('GetEmpOutput.html', result=result)
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).get_object(Key=emp_image_file_name_in_s3)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+     return render_template('GetEmpOutput.html', result=result, image_url=object_url)
 
 # Get Employee Done
 @app.route("/fetchdata/",methods=['GET','POST'])
 def getEmpDone():
     
     return render_template('GetEmp.html')
+
+# Delte Employee
+@app.route("/delemp", methods=['POST'])
+def delemp():
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+
+    insert_sql = "DELETE FROM employee WHERE emp_id = 'emp_id'"
+    cursor = db_conn.cursor()
+
+    try:
+
+        cursor.execute(insert_sql, (emp_id))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+        # Uplaod image file in S3 #
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).delete_object(Key=emp_image_file_name_in_s3)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('AddEmpOutput.html', name=emp_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
