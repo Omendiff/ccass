479419025049ddbadf2666debe6ca0987fc7c600
hongy <datedac@gmail.com>
2022-12-30 23:46:01 +0800

    s

--- EmpApp.py
+++ EmpApp.py
@@ -26,7 +26,7 @@ def home():
     return render_template('AddEmp.html')
 
 
-@app.route("/about", methods=['POST'])
+@app.route("/about", methods=['GET', 'POST'])
 def about():
     return render_template('About.html')
 
