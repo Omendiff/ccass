9a1b3091dc32cf0d6de7b26bfc747909d4c6f6f3
hongy <datedac@gmail.com>
2022-12-31 14:03:51 +0800

    sa

--- EmpApp.py
+++ EmpApp.py
@@ -132,6 +132,48 @@ def getEmp():
 
      return render_template('GetEmpOutput.html', result=result, image_url=object_url)
 
+# Get Employee Information for update page
+@app.route("/fetchdata2",methods=['GET','POST'])
+def getEmp2():
+     emp_id = request.form['emp_id']
+
+     select_stmt = "SELECT * FROM employee WHERE emp_id = %(emp_id)s"
+     cursor = db_conn.cursor()
+        
+     try:
+         cursor.execute(select_stmt, { 'emp_id': int(emp_id) })
+         for result in cursor:
+            print(result)
+
+     except Exception as e:
+        return str(e)
+        
+     finally:
+        cursor.close()
+
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+     return render_template('UpEmp.html', result=result, image_url=object_url)
+
 
 # Get Employee Done
 @app.route("/fetchdata/",methods=['GET','POST'])
