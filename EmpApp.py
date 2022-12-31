3f051000097e3cac967c1c398902f858ce768d4a
hongy <datedac@gmail.com>
2022-12-31 15:29:50 +0800

    ds

--- EmpApp.py
+++ EmpApp.py
@@ -181,65 +181,70 @@ def getEmpDone():
     return render_template('GetEmp.html')
 
 # update employee
-# @app.route("/upemp", methods=['POST'])
-# def upemp():
-#     emp_id = request.form['emp_id']
-#     first_name = request.form['first_name']
-#     last_name = request.form['last_name']
-#     pri_skill = request.form['pri_skill']
-#     location = request.form['location']
-#     emp_image_file = request.files['emp_image_file']
+@app.route("/upemp", methods=['POST'])
+def upemp():
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    pri_skill = request.form['pri_skill']
+    location = request.form['location']
+    emp_image_file = request.files['emp_image_file']
 
-#     #if no image uploaded
-#     if emp_image_file.filename == "":
-#         try:
-#             insert_sql = "UPDATE employee SET first_name=%s"
-#             cursor = db_conn.cursor()
-#             cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-#             db_conn.commit()
+    #if no image uploaded
+    if emp_image_file.filename == "":
+        try:
+            insert_sql = "UPDATE employee SET first_name= %s, last_name=%s, pri_skill=%s, location=%s where emp_id = %s"
+            cursor = db_conn.cursor()
+            cursor.execute(insert_sql, (first_name, last_name, pri_skill, location, emp_id))
+            db_conn.commit()
 
-#         except Exception as e:
-#             return str(e)
+        except Exception as e:
+            return str(e)
             
-#         finally:
-#             cursor.close()
+        finally:
+            cursor.close()
 
-#     #else got image upload, run image file query
-#     else:
-#         try:
+    #else got image upload, run image file query
+    else:
+        try:
             
-#             # Upload image file in S3 #
-#             image_file_name = "staff-id-" + str(name) + "_image_file"
-#             s3 = boto3.resource('s3')
+            # Upload image file in S3 #
+            image_file_name = "emp-id-" + str(emp_id) + "_image_file"
+            s3 = boto3.resource('s3')
 
-#             insert_sql = "UPDATE staff SET Name=%s, Email=%s, Phone=%s,RoleID=%s,DepartmentID=%s,Salary=%s,Status=%s WHERE StaffID=%s"
-#             cursor = db_conn.cursor()
-#             cursor.execute(insert_sql, (name, email, phone, role,department,salary,status,staffID))
-#             db_conn.commit()
+            insert_sql = "UPDATE staff SET first_name=%s, last_name=%s, pri_skill=%s, location=%s WHERE emp_id = %(emp_id)s"
+            cursor = db_conn.cursor()
+            cursor.execute(insert_sql, (first_name, last_name, pri_skill, location, emp_id))
+            db_conn.commit()
             
-#             print("Data inserted in MySQL RDS... uploading image to S3...")
-#             s3.Bucket(custombucket).put_object(Key=image_file_name, Body=image_file)
-#             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-#             s3_location = (bucket_location['LocationConstraint'])
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=image_file_name, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
             
-#             if s3_location is None:
-#                 s3_location = ''
-#             else:
-#                 s3_location = '-' + s3_location
-#                 object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(s3_location,custombucket,image_file_name)
-#         except Exception as e:
-#             return str(e)
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+                object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(s3_location,custombucket,image_file_name)
+        except Exception as e:
+            return str(e)
         
-#         finally:
-#             cursor.close()
+        finally:
+            cursor.close()
             
-#     titleData = "Data Updated"
-#     return render_template('StaffOutput.html',title=titleData)
+    titleData = "Data Updated"
+    return render_template('StaffOutput.html',title=titleData)
+
+# Update Employee Done
+@app.route("/upemp/",methods=['GET','POST'])
+def getEmpDone():
+    return render_template('GetEmp.html')
 
 # Delte Employee
 @app.route("/delemp", methods=['POST'])
 def delemp():
-    emp_id = request.form['emp_id']
+    emp_id = request.form['emp_id'] #--> emp_id got error
 
     insert_sql = "DELETE FROM employee WHERE emp_id = %s"
     cursor = db_conn.cursor()
